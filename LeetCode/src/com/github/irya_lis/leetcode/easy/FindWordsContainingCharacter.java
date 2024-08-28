d153b18a7e887bc270264fe3e0e6b9f4c022601c
Irina Rakova <lis.ovskaya1119@gmail.com>
2024-08-28 16:50:00 +0300

    .

---
+++ LeetCode/src/com/github/irya_lis/leetcode/easy/FindWordsContainingCharacter.java
@@ -0,0 +1,27 @@
+package com.github.irya_lis.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindWordsContainingCharacter {
+    public static void main(String[] args) {
+        String[] words = {"abc", "bcd", "aaaa", "cbc"};
+        System.out.println(findWordsContaining(words, 'a'));
+    }
+
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>(words.length);
+
+        for (int i = 0; i < words.length; i++) {
+            char[] charArray = words[i].toCharArray();
+            for (int k = 0; k < charArray.length; k++) {
+                if (charArray[k] == x) {
+                    result.add(i);
+                    break;
+                }
+            }
+
+        }
+        return result;
+    }
+}
